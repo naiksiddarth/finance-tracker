5ffe5faee9c360f72cbf653006131ca8f9779a7d
Siddarth Naik <siddarthnaik2131@gmail.com>
2026-09-13 21:47:46 +0530

    feat(shared): export TransactionType

--- packages/shared/src/enums/transactions/index.ts
+++ packages/shared/src/enums/transactions/index.ts
@@ -1,6 +1,8 @@
-export const TransactionTypes = {
+export const TransactionType = {
   DEBIT: "debit",
   CREDIT: "credit",
-}
+} as const
 
-export const TransactionTypesEnum = Object.values(TransactionTypes)
+export const TransactionTypeValues= Object.values(TransactionType)
+
+export type TransactionType = typeof TransactionTypeValues[number]
