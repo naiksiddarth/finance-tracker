b2f80b07ec5c37e3ca78a03562fa26428f5e33f3
Siddarth Naik <siddarthnaik2131@gmail.com>
2026-09-15 18:56:56 +0530

    feat(db): add date field in Transaction model

--- packages/db/src/models/transaction.ts
+++ packages/db/src/models/transaction.ts
@@ -7,23 +7,33 @@ export interface ITransaction {
   user: mongoose.Types.ObjectId
   amount: number
   type: TransactionType
+  date: Date
 }
 
-const TransactionSchema = new mongoose.Schema<ITransaction>({
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    required: true,
+const TransactionSchema = new mongoose.Schema<ITransaction>(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    amount: {
+      type: Number,
+      required: true,
+    },
+    type: {
+      type: String,
+      enum: TransactionTypeValues,
+      required: true,
+    },
+    date: {
+      type: Date,
+      required: true,
+    },
   },
-  amount: {
-    type: Number,
-    required: true,
-  },
-  type: {
-    type: String,
-    enum: TransactionTypeValues,
-    required: true,
-  },
-})
+  {
+    timestamps: true,
+  }
+)
 
 export const Transaction = mongoose.model("Transaction", TransactionSchema)
